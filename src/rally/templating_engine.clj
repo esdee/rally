1536da0dba23bcdb8feed41adfd5f6b554cb2f27
Shashy Dass <ShashyD@gmail.com>
2014-02-01 06:57:54 -0500

    DRY up code by declaring a var for the regex that is used

--- src/rally/templating_engine.clj
+++ src/rally/templating_engine.clj
@@ -2,6 +2,9 @@
 (ns rally.templating-engine
   (:require [clojure.string :as str]))
 
+;; This is the regex used to replace the template variables
+;; of the for ${x}
+(def ^:private regex #"\$\{([\w|-]+)\}")
 
 (defn template->string
   "Given a template string and a variable map, substitute each appearance of a
@@ -9,13 +12,13 @@
   variable map. If a key is referenced by the template but missing in the map,
   will throw an Exception"
   [template-string variable-map]
-  (let [keys-in-template (map last (re-seq #"\$\{([\w|-]+)\}" template-string))
+  (let [keys-in-template (map last (re-seq regex template-string))
         vals-for-template (map variable-map keys-in-template)]
     (if (some nil? vals-for-template)
       (let [missing-keys (filter #(nil? (variable-map %)) keys-in-template)]
         (throw (Exception. (format "Missing key(s) => %s"
                                     (str/join ", " missing-keys))))))
     (apply format
-           (str/replace template-string #"\$\{[\w|-]+\}" "%s")
+           (str/replace template-string regex "%s")
            vals-for-template)))
 
